5ca5f93ce5bd1a0ef6bee54a0d2ae36eb6dfd58b
Brennan Nugent <111827769+BrennanWNugent@users.noreply.github.com>
2022-09-27 20:44:37 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -26,12 +26,27 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const filteredArray = fruitsArray.filter((item) => {
+    return item.nutritions.calories < 50;
+  })
+
+  console.log(filteredArray);
+
   // TODO: Create a new HTML element to display your data
+
+  for (let i = 0; i < filteredArray.length; i++) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = filteredArray[i].name + " has " + filteredArray[i].nutritions.calories.toString() + " calories";
+    newElement.className = "fruit-item";
+
+    const existingElement = document.getElementById('fruit-container');
+    existingElement.append(newElement);
+  }
 
   // TODO: Append your new element to the page
 
